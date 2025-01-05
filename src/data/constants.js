4548db00df19e6adda8658b8efb0dfe9e47d6586
vigneshhaa <vigneshhaa@gmail.com>
2025-01-05 12:36:27 +0530

    resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "Experienced Team Lead and Full Stack Developer with 6 years of expertise in designing, developing, and managing complex web application. Proficient in leading cross-functional teams, driving Agile development, and delivering high-quality software solutions.",
   github: "https://github.com/vigneshhaa",
   resume:
-    "https://drive.google.com/file/d/1JmECqrjNUREtv9yCmmtvET9WpVFXlI1i/view?usp=sharing",
+    "https://drive.google.com/file/d/1zk0ZTAzsWbCR2SZ2J6ukkESf15VQakNs/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/vigneshwaran-a-10b7521a7/",
 };
 
